5f0665804f97e6042f876baaf395d7a05890dc92
Rymuu <96126158+Rymuu@users.noreply.github.com>
2022-05-16 18:02:19 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+  const [token, setToken] = useState(typeof window !== "undefined" ? localStorage.getItem("jwt") : []);
+  const [isLogged, setIsLogged] = useState(false);
 
-  return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">YnovEvent</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="home">Home</Nav.Link>
-            <Nav.Link href="event">Event</Nav.Link>
-            <Nav.Link href="admin">Admin</Nav.Link>
-            <NavDropdown title="Events" id="nav-dropdown">
-              <NavDropdown.Item eventKey="4.1">Create an event</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item eventKey="4.2">My events</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Nav.Link href="login">Login</Nav.Link>
-            <Nav.Link href="register">Register</Nav.Link>
-            <Nav.Link href="register">Account</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <br />
-    </>
-  );
+
+
+const logoff = () => {
+  setIsLogged(false);
+  localStorage.setItem("jwt", "vide");
+  router.push("/login");
+}
+
+
+return (
+  <>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">YnovEvent</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="home">Home</Nav.Link>
+          <Nav.Link href="event">Event</Nav.Link>
+          <Nav.Link href="admin">Admin</Nav.Link>
+          <NavDropdown title="Events" id="nav-dropdown">
+            <NavDropdown.Item eventKey="4.1">Create an event</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item eventKey="4.2">My events</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+          {true? (
+            <>
+              <Nav.Link href="account"><AccountCircleOutlinedIcon /></Nav.Link>
+              <Nav.Link href="login"><LogoutOutlinedIcon onClick={() => logoff()} /></Nav.Link>
+            </>
+          )
+            : (
+              <>
+                <Nav.Link href="login">Login</Nav.Link>
+                <Nav.Link href="register">Register</Nav.Link>
+              </>
+            )
+          }
+
+
+        </Nav>
+      </Container>
+    </Navbar >
+    <br />
+  </>
+);
 };
 
 export default Header;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,33 +1,70 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, CardGroup, Carousel } from "react-bootstrap";
 
 
 
 const Index = () => {
 
   return (
-    <div className="page__home">
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col xs lg="2">
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
+    <div className="page__home" >
+      {
+    document.body.style.backgroundColor = "red"
+}
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="src/public/salad-g4929438b0_1920.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="src/public/salad-g4929438b0_1920.jpg"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="src/public/salad-g4929438b0_1920.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+
+      <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="./public/salad-g4929438b0_1920.jpg" />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Card title</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit longer.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
               </Card.Body>
             </Card>
           </Col>
-          <Col md="auto">Variable width content</Col>
-          <Col xs lg="2">
-            3 of 3
-          </Col>
-        </Row>
-      </Container>
+        ))}
+      </Row>
     </div>
   );
 };
