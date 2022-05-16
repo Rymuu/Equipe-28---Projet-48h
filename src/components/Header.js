640dc118263dd9d7b156a637e66ce254597b73e5
Rymuu <96126158+Rymuu@users.noreply.github.com>
2022-05-16 14:41:32 +0200

    Header NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Form, FormControl, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const Header = () => {
 
@@ -9,10 +9,18 @@ const Header = () => {
         <Container>
           <Navbar.Brand href="#home">YnovEvent</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">List</Nav.Link>
-            <Nav.Link href="#pricing">Admin</Nav.Link>
-            <Nav.Link href="#pricing">Connexion</Nav.Link>
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="event">Event</Nav.Link>
+            <Nav.Link href="admin">Admin</Nav.Link>
+            <Nav.Link href="login">Login</Nav.Link>
+            <Nav.Link href="register">Register</Nav.Link>
+            <NavDropdown title="Organiser" id="nav-dropdown">
+              <NavDropdown.Item eventKey="4.1">créer</NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Container>
       </Navbar>
